6c9b4776053507ccdd210eca3bf38d098ec798e0
ilush88 <ilyakop1998@gmail.com>
2025-12-14 13:21:55 +0500

    karkas

--- rusikrutoi/rusikrutoi.cpp
+++ rusikrutoi/rusikrutoi.cpp
@@ -1,8 +1,33 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 using namespace std;
-int main()
-{
-    cout << "I love jews";
+vector<string> readStringsFromFile(const string& filename) {
+
+    return vector<string>();
 }
 
+void printStrings(const vector<string>& strings) {
+
+}
+
+void writeStringsToFile(const vector<string>& strings, const string& filename) {
+
+}
+
+int main() {
+
+    string inputFilename = "input.txt";
+
+    string outputFilename = "output.txt";
+
+    vector<string> strings = readStringsFromFile(inputFilename);
+
+    printStrings(strings);
+
+    writeStringsToFile(strings, outputFilename);
+
+    return 0;
+}
